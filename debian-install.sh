ddd23d953dc1ec70c60ea9f327706ac9b08313dd
wangyi2005 <wangyi2005@users.noreply.github.com>
2019-02-16 14:17:43 +0800

    Update debian-install.sh

--- debian-install.sh
+++ debian-install.sh
@@ -3,18 +3,18 @@ apt-get update
 apt-get -y upgrade
 apt-get -y dist-upgrade
 sed -i 's/stretch/buster/g' /etc/apt/sources.list
-cat etc/apt/sources.list
+#cat etc/apt/sources.list
 apt-get update
 apt-get -y upgrade
 apt-get -y dist-upgrade
-cat /etc/os-release
+#cat /etc/os-release
 
 # ip forward bbr
 echo "net.ipv4.ip_forward = 1" >> /etc/sysctl.conf 
 echo "net.core.default_qdisc=fq" >> /etc/sysctl.conf
 echo "net.ipv4.tcp_congestion_control=bbr" >> /etc/sysctl.conf
 sysctl -p 
-#smod | grep bbr
+#lsmod | grep bbr
 
 #v2ray h2,ws,tcp,quic
 apt-get install curl -y
@@ -40,8 +40,8 @@ systemctl start dnsmasq
 
 #set ip rules
 iptables -t nat -A POSTROUTING -s 192.168.18.0/24 -o eth0 -j MASQUERADE
-iptables -A FORWARD -i vpns+ -j ACCEPT 
-iptables -A FORWARD -o vpns+ -j ACCEPT
+#iptables -A FORWARD -i vpns+ -j ACCEPT 
+#iptables -A FORWARD -o vpns+ -j ACCEPT
 iptables -A FORWARD -p tcp -m tcp --tcp-flags SYN,RST SYN -j TCPMSS --clamp-mss-to-pmtu
 apt-get install iptables-persistent
 #cat /etc/iptables/rules.v4
@@ -53,8 +53,13 @@ wget -O /etc/ocserv/ocserv.conf   https://raw.githubusercontent.com/wangyi2005/o
 wget -O /etc/ocserv/ca-cert.pem   https://raw.githubusercontent.com/wangyi2005/ocserv/master/ca-cert.pem
 wget -O /etc/ocserv/server-cert.pem  https://raw.githubusercontent.com/wangyi2005/ocserv/master/server-cert.pem
 wget -O /etc/ocserv/server-key.pem  https://raw.githubusercontent.com/wangyi2005/ocserv/master/server-key.pem
+wget -O /etc/systemd/system/ocserv.service  https://raw.githubusercontent.com/wangyi2005/ocserv/master/ocserv.service
 #cat /lib/systemd/system/ocserv.service（uncommet require 和 also）
 
 systemctl enable ocserv
-systemctl start ocserv
+#systemctl start ocserv
 #systemctl status ocserv
+#systemctl stop ocserv.socket
+sudo systemctl disable ocserv.socket
+sudo systemctl start ocserv.service
+reboot
