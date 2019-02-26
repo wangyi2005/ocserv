5e7305db996450b94e298fc607b34d170aaf215f
wangyi2005 <wangyi2005@users.noreply.github.com>
2019-02-26 12:11:30 +0800

    Update debian-install.sh

--- debian-install.sh
+++ debian-install.sh
@@ -19,13 +19,12 @@ apt-get -y dist-upgrade
 echo "net.ipv4.ip_forward = 1" >> /etc/sysctl.conf 
 echo "net.core.default_qdisc=fq" >> /etc/sysctl.conf
 echo "net.ipv4.tcp_congestion_control=bbr" >> /etc/sysctl.conf
-# echo 'net.core.wmem_max=12582912' >> /etc/sysctl.conf
-# echo 'net.core.rmem_max=12582912' >> /etc/sysctl.conf
-# echo 'net.ipv4.tcp_rmem= 10240 87380 12582912' >> /etc/sysctl.conf
-# echo 'net.ipv4.tcp_wmem= 10240 87380 12582912' >> /etc/sysctl.conf
-# echo 'net.ipv4.udp_mem = 65536 131072 262144' >> /etc/sysctl.conf
-# echo 'net.core.netdev_max_backlog = 5000' >> /etc/sysctl.conf
-# eth0 txqueuelen 10000
+# echo "net.core.rmem_max = 67108864" >> /etc/sysctl.conf
+# echo "net.core.wmem_max = 67108864" >> /etc/sysctl.conf
+# echo "net.ipv4.tcp_rmem = 4096 87380 33554432" >> /etc/sysctl.conf
+# echo "net.ipv4.tcp_wmem = 4096 65536 33554432" >> /etc/sysctl.conf
+# echo "net.ipv4.tcp_mtu_probing=1" >> /etc/sysctl.conf
+# echo "net.core.netdev_max_backlog = 5000" >> /etc/sysctl.conf
 sysctl -p 
 #lsmod | grep bbr
 
