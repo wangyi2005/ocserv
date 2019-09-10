af867a58a502bdfc0c2e72160ed75b545e77ef0c
wangyi2005 <wangyi2005@users.noreply.github.com>
2019-09-10 08:27:52 +0800

    Update centos-install.sh

--- centos-install.sh
+++ centos-install.sh
@@ -7,15 +7,15 @@ yum -y install net-tools
 rpm -e epel-release-7-11.noarch
 yum install https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm -y
 curl -Lo /etc/yum.repos.d/wireguard.repo https://copr.fedorainfracloud.org/coprs/jdoss/wireguard/repo/epel-7/jdoss-wireguard-epel-7.repo
-yum repolist
+#yum repolist
 yum install epel-release -y
 
 #v2ray tcp h2 8443 quic 4443
-bash <(curl -L -s https://install.direct/go.sh)
-wget -O /etc/v2ray/config.json  https://raw.githubusercontent.com/wangyi2005/ocserv/master/v2ray_server.json
-wget -O /etc/v2ray/wy_cer.pem   https://raw.githubusercontent.com/wangyi2005/ocserv/master/wy_cer.pem 
-wget -O /etc/v2ray/wy_key.pem   https://raw.githubusercontent.com/wangyi2005/ocserv/master/wy_key.pem 
-systemctl enable v2ray
+#bash <(curl -L -s https://install.direct/go.sh)
+#wget -O /etc/v2ray/config.json  https://raw.githubusercontent.com/wangyi2005/ocserv/master/v2ray_server.json
+#wget -O /etc/v2ray/wy_cer.pem   https://raw.githubusercontent.com/wangyi2005/ocserv/master/wy_cer.pem 
+#wget -O /etc/v2ray/wy_key.pem   https://raw.githubusercontent.com/wangyi2005/ocserv/master/wy_key.pem 
+#systemctl enable v2ray
 #systemctl start v2ray
 
 # install dingo port 5353 CDN-china-domains
@@ -37,12 +37,12 @@ systemctl enable dnsmasq
 
 # install PowerDNS Recursor - https://repo.powerdns.com 
 #yum -y install https://repo.powerdns.com/centos/x86_64/7Server/rec-42/pdns-recursor-4.2.0-1pdns.el7.x86_64.rpm
-wget -O /etc/yum.repos.d/powerdns-rec-42.repo https://repo.powerdns.com/repo-files/centos-rec-42.repo
-yum -y install pdns-recursor
+#wget -O /etc/yum.repos.d/powerdns-rec-42.repo https://repo.powerdns.com/repo-files/centos-rec-42.repo
+#yum -y install pdns-recursor
 #nano /etc/pdns-recursor/recursor.conf
-wget -O /etc/pdns-recursor/recursor.conf   https://raw.githubusercontent.com/wangyi2005/ocserv/master/pdns-recursor.conf 
-systemctl enable pdns-recursor
-systemctl start pdns-recursor
+#wget -O /etc/pdns-recursor/recursor.conf   https://raw.githubusercontent.com/wangyi2005/ocserv/master/pdns-recursor.conf 
+#systemctl enable pdns-recursor
+#systemctl start pdns-recursor
 
 # install iptables
 systemctl stop firewalld.service
@@ -75,15 +75,15 @@ tuned-adm profile network-throughput
 #sysctl -p
 
 # ocserv 0.12.3 tcp 443 udp 443
-yum install ocserv -y 
+#yum install ocserv -y 
 #yum --enablerepo=epel-testing install ocserv -y 
 #yum install https://dl.fedoraproject.org/pub/epel/7/x86_64/Packages/o/ocserv-0.12.3-1.el7.x86_64.rpm -y
 #ocserv -v
-wget -O /etc/ocserv/ocserv.conf   https://raw.githubusercontent.com/wangyi2005/ocserv/master/ocserv.conf
-wget -O /etc/ocserv/ca-cert.pem   https://raw.githubusercontent.com/wangyi2005/ocserv/master/ca-cert.pem
-wget -O /etc/ocserv/server-cert.pem  https://raw.githubusercontent.com/wangyi2005/ocserv/master/server-cert.pem
-wget -O /etc/ocserv/server-key.pem  https://raw.githubusercontent.com/wangyi2005/ocserv/master/server-key.pem
-systemctl enable ocserv
+#wget -O /etc/ocserv/ocserv.conf   https://raw.githubusercontent.com/wangyi2005/ocserv/master/ocserv.conf
+#wget -O /etc/ocserv/ca-cert.pem   https://raw.githubusercontent.com/wangyi2005/ocserv/master/ca-cert.pem
+#wget -O /etc/ocserv/server-cert.pem  https://raw.githubusercontent.com/wangyi2005/ocserv/master/server-cert.pem
+#wget -O /etc/ocserv/server-key.pem  https://raw.githubusercontent.com/wangyi2005/ocserv/master/server-key.pem
+#systemctl enable ocserv
 
 # wireguard 
 mkdir /etc/wireguard 
