1806bd475c732c06fb5bb6cf0eefaff73bdbe7d6
wangyi2005 <wangyi2005@users.noreply.github.com>
2019-09-10 14:45:24 +0800

    Update ubuntu-install.sh

--- ubuntu-install.sh
+++ ubuntu-install.sh
@@ -21,6 +21,10 @@ echo "net.ipv4.ip_forward = 1" >> /etc/sysctl.conf
 echo "net.core.default_qdisc=fq" >> /etc/sysctl.conf
 echo "net.ipv4.tcp_congestion_control=bbr" >> /etc/sysctl.conf
 
+# Disable IPv6
+#echo "net.ipv6.conf.all.disable_ipv6 = 1" >> /etc/sysctl.conf
+#echo "net.ipv6.conf.default.disable_ipv6 = 1" >> /etc/sysctl.conf
+
 apt-get install tuned -y
 tuned-adm list
 tuned-adm profile network-throughput
