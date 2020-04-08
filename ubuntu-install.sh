835d1564dece9d45d4334d1c9ad625a80eea8162
wangyi2005 <wangyi2005@users.noreply.github.com>
2020-04-08 21:53:24 +0800

    Update ubuntu-install.sh

--- ubuntu-install.sh
+++ ubuntu-install.sh
@@ -4,13 +4,22 @@
 #nano /etc/update-manager/release-upgrades (Prompt=normal)
 #do-release-upgrade
 
+# update kernel  https://kernel.ubuntu.com/~kernel-ppa/mainline/
+#cd /tmp
+#wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.6.2/linux-headers-5.6.2-050602_5.6.2-050602.202004020822_all.deb
+#wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.6.2/linux-headers-5.6.2-050602-generic_5.6.2-050602.202004020822_amd64.deb
+#wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.6.2/linux-image-unsigned-5.6.2-050602-generic_5.6.2-050602.202004020822_amd64.deb
+#wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.6.2/linux-modules-5.6.2-050602-generic_5.6.2-050602.202004020822_amd64.deb
+#dpkg -i *.deb
+
 # install wireguard
 apt install software-properties-common -y
 #apt-get install curl -y
 #19.10 still 
 add-apt-repository ppa:wireguard/wireguard 
 apt-get update -y
-apt-get install wireguard -y
+#kernal 5.6 only install wireguard-tools
+apt-get install wireguard 
 wget -O /etc/wireguard/wg0.conf   https://raw.githubusercontent.com/wangyi2005/ocserv/master/wg0.conf
 chmod 600 /etc/wireguard/wg0.conf
 systemctl enable wg-quick@wg0
@@ -23,14 +32,6 @@ echo "net.ipv4.tcp_congestion_control=bbr" >> /etc/sysctl.conf
 # Disable IPv6
 #echo "net.ipv6.conf.all.disable_ipv6 = 1" >> /etc/sysctl.conf
 #echo "net.ipv6.conf.default.disable_ipv6 = 1" >> /etc/sysctl.conf
-
-#apt-get install tuned -y
-#tuned-adm list
-#tuned-adm profile network-throughput
-#tuned-adm profile network-latency
-#tuned-adm profile latency-performance
-#tuned-adm profile throughput-performance
-#tuned-adm active
 
 # install dingo port 5353 CDN-china-domains
 #wget -O /usr/bin/dingo  https://raw.githubusercontent.com/wangyi2005/ocserv/master/dingo-linux-amd64
@@ -47,17 +48,12 @@ wget -O /etc/dnsmasq.conf   https://raw.githubusercontent.com/wangyi2005/ocserv/
 systemctl enable dnsmasq
 
 # install sniproxy 0.6.0 require 19.10
-apt-get install sniproxy -y
+#apt-get install sniproxy -y
+#https://packages.ubuntu.com/disco/amd64/sniproxy/download
+wget -O /usr/bin/sniproxy.deb https://raw.githubusercontent.com/wangyi2005/ocserv/master/sniproxy_0.6.0-1_amd64.deb
+dpkg -i /usr/bin/sniproxy.deb
 wget -O /etc/sniproxy.conf   https://raw.githubusercontent.com/wangyi2005/ocserv/master/sniproxy.conf
 wget -O /etc/systemd/system/sniproxy.service   https://raw.githubusercontent.com/wangyi2005/ocserv/master/sniproxy.service
 systemctl enable sniproxy
-
-# update kernel  https://kernel.ubuntu.com/~kernel-ppa/mainline/
-#cd ~ && mkdir newkernel && cd newkernel
-#wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.6.2/linux-headers-5.6.2-050602_5.6.2-050602.202004020822_all.deb
-#wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.6.2/linux-headers-5.6.2-050602-generic_5.6.2-050602.202004020822_amd64.deb
-#wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.6.2/linux-image-unsigned-5.6.2-050602-generic_5.6.2-050602.202004020822_amd64.deb
-#wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.6.2/linux-modules-5.6.2-050602-generic_5.6.2-050602.202004020822_amd64.deb
-#dpkg -i *.deb
 
 reboot
