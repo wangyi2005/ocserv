e3ee91c435226a660f76c4461d55b64ea023732a
wangyi2005 <wangyi2005@users.noreply.github.com>
2020-04-08 12:44:25 +0800

    Update debian-install.sh

--- debian-install.sh
+++ debian-install.sh
@@ -1,12 +1,12 @@
 #debian 9 to 10 
 apt-get update
 apt-get -y upgrade
-apt-get -y dist-upgrade
-sed -i 's/stretch/buster/g' /etc/apt/sources.list
+#apt-get -y dist-upgrade
+#sed -i 's/stretch/buster/g' /etc/apt/sources.list
 #cat etc/apt/sources.list
-apt-get update
-apt-get -y upgrade
-apt-get -y dist-upgrade
+#apt-get update
+#apt-get -y upgrade
+#apt-get -y dist-upgrade
 #cat /etc/os-release
 
 #ubuntu 18 to 19
@@ -25,15 +25,15 @@ echo "net.ipv4.tcp_congestion_control=bbr" >> /etc/sysctl.conf
 # echo "net.ipv4.tcp_wmem = 4096 65536 33554432" >> /etc/sysctl.conf
 # echo "net.ipv4.tcp_mtu_probing=1" >> /etc/sysctl.conf
 # echo "net.core.netdev_max_backlog = 5000" >> /etc/sysctl.conf
-sysctl -p 
+#sysctl -p 
 #lsmod | grep bbr
 
 #v2ray h2,ws,tcp,quic
-apt-get install curl -y
-bash <(curl -L -s https://install.direct/go.sh)
-wget -O /etc/v2ray/config.json  https://raw.githubusercontent.com/wangyi2005/ocserv/master/v2ray_server.json
-wget -O /etc/v2ray/wy_cer.pem   https://raw.githubusercontent.com/wangyi2005/ocserv/master/wy_cer.pem 
-wget -O /etc/v2ray/wy_key.pem   https://raw.githubusercontent.com/wangyi2005/ocserv/master/wy_key.pem 
+#apt-get install curl -y
+#bash <(curl -L -s https://install.direct/go.sh)
+#wget -O /etc/v2ray/config.json  https://raw.githubusercontent.com/wangyi2005/ocserv/master/v2ray_server.json
+#wget -O /etc/v2ray/wy_cer.pem   https://raw.githubusercontent.com/wangyi2005/ocserv/master/wy_cer.pem 
+#wget -O /etc/v2ray/wy_key.pem   https://raw.githubusercontent.com/wangyi2005/ocserv/master/wy_key.pem 
 #cat /etc/v2ray/config.json
 #cat /etc/v2ray/wy_cer.pem 
 #cat /etc/v2ray/wy_key.pem 
@@ -43,43 +43,57 @@ wget -O /etc/v2ray/wy_key.pem   https://raw.githubusercontent.com/wangyi2005/ocs
 # install dnsmasq
 apt-get install dnsmasq -y
 wget -O /etc/dnsmasq.conf   https://raw.githubusercontent.com/wangyi2005/ocserv/master/dnsmasq.conf
-wget -O /etc/resolv.dnsmasq.conf  https://raw.githubusercontent.com/wangyi2005/ocserv/master/resolv.dnsmasq.conf
+#wget -O /etc/resolv.dnsmasq.conf  https://raw.githubusercontent.com/wangyi2005/ocserv/master/resolv.dnsmasq.conf
 #wget -O /etc/dnsmasq.d/china-domains.conf  https://raw.githubusercontent.com/wangyi2005/ocserv/master/china-domains.conf
 wget -O /etc/dnsmasq.d/china-domains.conf  https://raw.githubusercontent.com/wangyi2005/ocserv/master/china-domains-dingo.conf
+systemctl enable dnsmasq
 #cat /etc/dnsmasq.conf
 #cat /etc/resolv.dnsmasq.conf
-systemctl enable dnsmasq
+#systemctl enable dnsmasq
 #systemctl start dnsmasq
 
 # install dingo port 5353 CDN-china-domains
-wget -O /usr/bin/dingo  https://raw.githubusercontent.com/wangyi2005/ocserv/master/dingo-linux-amd64
-#wget -O /usr/bin/dingo  https://github.com/pforemski/dingo/releases/download/0.13/dingo-linux-amd64
+#wget -O /usr/bin/dingo  https://raw.githubusercontent.com/wangyi2005/ocserv/master/dingo-linux-amd64
+wget -O /usr/bin/dingo  https://github.com/pforemski/dingo/releases/download/0.13/dingo-linux-amd64
 chmod +x /usr/bin/dingo
-wget -O /etc/systemd/system/dingo.service   https://raw.githubusercontent.com/wangyi2005/ocserv/master/dingo.service
-systemctl enable dingo
+wget -O /etc/systemd/system/dingo-ecs-us.service   https://raw.githubusercontent.com/wangyi2005/ocserv/master/dingo-ecs-us.service
+wget -O /etc/systemd/system/dingo-ecs-cn.service   https://raw.githubusercontent.com/wangyi2005/ocserv/master/dingo-ecs-cn.service
+systemctl enable dingo-ecs-us
+systemctl enable dingo-ecs-cn
 
 #set ip rules
-iptables -t nat -A POSTROUTING -s 192.168.18.0/24 -o eth0 -j MASQUERADE
+#iptables -t nat -A POSTROUTING -s 192.168.18.0/24 -o eth0 -j MASQUERADE
 #iptables -A FORWARD -i vpns+ -j ACCEPT 
 #iptables -A FORWARD -o vpns+ -j ACCEPT
-iptables -A FORWARD -p tcp -m tcp --tcp-flags SYN,RST SYN -j TCPMSS --clamp-mss-to-pmtu
-apt-get install iptables-persistent -y
+#iptables -A FORWARD -p tcp -m tcp --tcp-flags SYN,RST SYN -j TCPMSS --clamp-mss-to-pmtu
+#apt-get install iptables-persistent -y
 #cat /etc/iptables/rules.v4
 
 # install ocserv 0.12.2
-apt-get install ocserv -y
+#apt-get install ocserv -y
 #apt autoremove
-wget -O /etc/ocserv/ocserv.conf   https://raw.githubusercontent.com/wangyi2005/ocserv/master/ocserv.conf
-wget -O /etc/ocserv/ca-cert.pem   https://raw.githubusercontent.com/wangyi2005/ocserv/master/ca-cert.pem
-wget -O /etc/ocserv/server-cert.pem  https://raw.githubusercontent.com/wangyi2005/ocserv/master/server-cert.pem
-wget -O /etc/ocserv/server-key.pem  https://raw.githubusercontent.com/wangyi2005/ocserv/master/server-key.pem
+#wget -O /etc/ocserv/ocserv.conf   https://raw.githubusercontent.com/wangyi2005/ocserv/master/ocserv.conf
+#wget -O /etc/ocserv/ca-cert.pem   https://raw.githubusercontent.com/wangyi2005/ocserv/master/ca-cert.pem
+#wget -O /etc/ocserv/server-cert.pem  https://raw.githubusercontent.com/wangyi2005/ocserv/master/server-cert.pem
+#wget -O /etc/ocserv/server-key.pem  https://raw.githubusercontent.com/wangyi2005/ocserv/master/server-key.pem
 #wget -O /etc/systemd/system/ocserv.service  https://raw.githubusercontent.com/wangyi2005/ocserv/master/ocserv.service
 #cat /lib/systemd/system/ocserv.service（uncommet require 和 also）
-
-systemctl enable ocserv
+#systemctl enable ocserv
 #systemctl start ocserv
 #systemctl status ocserv
 #systemctl stop ocserv.socket
 #systemctl disable ocserv.socket
 #systemctl start ocserv.service
+
+# install wireguard
+apt install wireguard
+wget -O /etc/wireguard/wg0.conf   https://raw.githubusercontent.com/wangyi2005/ocserv/master/wg0.conf
+chmod 600 /etc/wireguard/wg0.conf
+systemctl enable wg-quick@wg0
+
+# install sniproxy
+apt-get install sniproxy
+wget -O /etc/sniproxy.conf   https://raw.githubusercontent.com/wangyi2005/ocserv/master/sniproxy.conf
+systemctl enable sniproxy
+
 reboot
