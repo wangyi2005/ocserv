93ea2d0b9b767bf9e01f8ede948b2243e247cb1e
wangyi2005 <wangyi2005@users.noreply.github.com>
2020-06-17 19:14:43 +0800

    Create acme-wild.sh

---
+++ acme-wild.sh
@@ -0,0 +1,14 @@
+curl https://get.acme.sh | sh
+exit
+export CF_Key="88851c6e592589879d8fdc1e534d2f0a20784"  
+export CF_Email="tanghb@outlook.com" 
+acme.sh --issue --dns dns_cf -d wangyi.cf -d *.wangyi.cf
+acme.sh --upgrade
+
+
+/root/.acme.sh/wangyi.cf/wangyi.cf.cer
+/root/.acme.sh/wangyi.cf/wangyi.cf.key
+/root/.acme.sh/wangyi.cf/ca.cer
+/root/.acme.sh/wangyi.cf/fullchain.cer
+
+
