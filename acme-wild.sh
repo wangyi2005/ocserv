9dfc2092b93ee4385da81b41bd2a252306e77de9
wangyi2005 <wangyi2005@users.noreply.github.com>
2020-06-17 20:58:29 +0800

    Update acme-wild.sh

--- acme-wild.sh
+++ acme-wild.sh
@@ -6,17 +6,22 @@ curl https://get.acme.sh | sh
 acme.sh --uninstall
 #先退出终端再次进入
 exit
-export CF_Key="88851c6e592589879d8fdc1e534d2f0a20784"  
-export CF_Email="tanghb@outlook.com" 
+#cloudflare 不再支持 cf,ml等域名的dns api
+#export CF_Key="88851c6e592589879d8fdc1e534d2f0a20784"  
+#export CF_Email="tanghb@outlook.com" 
 #查看变量
 env
 acme.sh --issue --dns dns_cf -d wangyi.cf -d *.wangyi.cf
 #更新acme.sh
 acme.sh --upgrade
 
+# 手动模式 https://github.com/acmesh-official/acme.sh/wiki/dns-manual-mode
+acme.sh --issue --dns -d vpn.wangyi.cf --yes-I-know-dns-manual-mode-enough-go-ahead-please
+# 在 cloudflare 添加 提示 的 TXT 记录。
+acme.sh --renew --dns -d vpn.wangyi.cf --yes-I-know-dns-manual-mode-enough-go-ahead-please
 
-/root/.acme.sh/wangyi.cf/wangyi.cf.cer
-/root/.acme.sh/wangyi.cf/wangyi.cf.key
+/root/.acme.sh/wangyi.cf/vpn.wangyi.cf.cer
+/root/.acme.sh/wangyi.cf/vpn.wangyi.cf.key
 /root/.acme.sh/wangyi.cf/ca.cer
 /root/.acme.sh/wangyi.cf/fullchain.cer
 
