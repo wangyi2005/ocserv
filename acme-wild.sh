04f891876ec9b31f3a4b3ce6f9b1e23ccefc2029
wangyi2005 <wangyi2005@users.noreply.github.com>
2020-06-17 20:11:58 +0800

    Update acme-wild.sh

--- acme-wild.sh
+++ acme-wild.sh
@@ -1,8 +1,11 @@
 #https://github.com/acmesh-official/acme.sh
 curl https://get.acme.sh | sh
+#先退出终端再次进入
 exit
 export CF_Key="88851c6e592589879d8fdc1e534d2f0a20784"  
 export CF_Email="tanghb@outlook.com" 
+#查看变量
+env
 acme.sh --issue --dns dns_cf -d wangyi.cf -d *.wangyi.cf
 #更新acme.sh
 acme.sh --upgrade
