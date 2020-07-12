cfe861b19e2ba4069b0fc634f52f74e4ee7221dd
wangyi2005 <wangyi2005@users.noreply.github.com>
2020-07-12 22:57:38 +0800

    Update acme-wild.sh

--- acme-wild.sh
+++ acme-wild.sh
@@ -18,6 +18,7 @@ acme.sh --upgrade
 # 手动模式 https://github.com/acmesh-official/acme.sh/wiki/dns-manual-mode
 /root/.acme.sh/acme.sh --issue --dns -d vpn.wangyi.cf --yes-I-know-dns-manual-mode-enough-go-ahead-please
 /root/.acme.sh/acme.sh --issue --dns -d cn2.wangyi.cf --yes-I-know-dns-manual-mode-enough-go-ahead-please
+#ECC 证书
 /root/.acme.sh/acme.sh --issue --dns -d gia.wangyi.cf --yes-I-know-dns-manual-mode-enough-go-ahead-please -k ec-256
 # 在 cloudflare 添加 提示 的 TXT 记录。
 type：TXT
@@ -25,6 +26,8 @@ name：_acme-challenge.cn2
 content:E8RGF1QkE3eZHq0WxI8mdqwMSvbU842KUfth4vyvI2Y
 
 acme.sh --renew --dns -d vpn.wangyi.cf --yes-I-know-dns-manual-mode-enough-go-ahead-please
+#ECC 证书
+acme.sh --renew --dns -d gia.wangyi.cf --yes-I-know-dns-manual-mode-enough-go-ahead-please -k ec256
 
 /root/.acme.sh/wangyi.cf/vpn.wangyi.cf.cer
 /root/.acme.sh/wangyi.cf/vpn.wangyi.cf.key
