6a65a79c7814c0701d6ce2d937b54d6c4dcf2667
wangyi2005 <wangyi2005@users.noreply.github.com>
2022-04-04 13:23:14 +0800

    Update ubuntu-install.sh

--- ubuntu-install.sh
+++ ubuntu-install.sh
@@ -1,39 +1,26 @@
-#ubuntu 18 to 19
-#apt update && apt upgrade && apt dist-upgrade && apt autoremove
-#apt-get install update-manager-core
-#nano /etc/update-manager/release-upgrades (Prompt=normal)
-#do-release-upgrade
-
-# update kernel  https://kernel.ubuntu.com/~kernel-ppa/mainline/
-#cd /tmp
-#wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.17/amd64/linux-headers-5.17.0-051700-generic_5.17.0-051700.202203202130_amd64.deb
-#wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.17/amd64/linux-headers-5.17.0-051700_5.17.0-051700.202203202130_all.deb
-#wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.17/amd64/linux-image-unsigned-5.17.0-051700-generic_5.17.0-051700.202203202130_amd64.deb
-#wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.17/amd64/linux-modules-5.17.0-051700-generic_5.17.0-051700.202203202130_amd64.deb
-#dpkg -i *.deb
+#ubuntu 20.04 oraclecloud
 
 apt-get update
 apt-get upgrade
-apt install software-properties-common -y
-# install wireguard
-#apt-get install curl -y
-#19.10 still 
-add-apt-repository ppa:wireguard/wireguard 
-apt-get update -y
-#kernal 5.6 only install wireguard-tools
-apt-get install wireguard 
-wget -O /etc/wireguard/wg0.conf   https://raw.githubusercontent.com/wangyi2005/ocserv/master/wg0.conf
-chmod 600 /etc/wireguard/wg0.conf
-systemctl enable wg-quick@wg0
+
+
+wget https://www.infradead.org/ocserv/download/ocserv-1.1.6.tar.xz
+tar Jxf ocserv-1.1.6.tar.xz
+cd ocserv-1.1.6
+./configure --prefix=/usr/local/ocserv
+make&&make install
+
+cp doc/profile.xml /usr/local/ocserv
+mkdir -p /etc/ocserv
+wget -O /etc/ocserv/ocserv.conf   https://raw.githubusercontent.com/wangyi2005/ocserv/master/ocserv.conf
+wget -O /etc/ocserv/ca-cert.pem   https://raw.githubusercontent.com/wangyi2005/ocserv/master/ca-cert.pem
+wget -O /etc/ocserv/server-cert.pem  https://raw.githubusercontent.com/wangyi2005/ocserv/master/server-cert.pem
+wget -O /etc/ocserv/server-key.pem  https://raw.githubusercontent.com/wangyi2005/ocserv/master/server-key.pem
 
 # ip forward bbr
 echo "net.ipv4.ip_forward = 1" >> /etc/sysctl.conf 
 echo "net.core.default_qdisc=fq" >> /etc/sysctl.conf
 echo "net.ipv4.tcp_congestion_control=bbr" >> /etc/sysctl.conf
-
-# Disable IPv6
-#echo "net.ipv6.conf.all.disable_ipv6 = 1" >> /etc/sysctl.conf
-#echo "net.ipv6.conf.default.disable_ipv6 = 1" >> /etc/sysctl.conf
 
 # install dingo port 5353 CDN-china-domains
 #wget -O /usr/bin/dingo  https://raw.githubusercontent.com/wangyi2005/ocserv/master/dingo-linux-amd64
