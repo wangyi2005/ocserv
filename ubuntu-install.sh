33eab57ca86e36e51bb7c06e02e5b99f23985762
wangyi2005 <wangyi2005@users.noreply.github.com>
2022-04-04 13:36:48 +0800

    Update ubuntu-install.sh

--- ubuntu-install.sh
+++ ubuntu-install.sh
@@ -3,6 +3,15 @@
 apt-get update
 apt-get upgrade
 
+apt-get install -y libgnutls28-dev libev-dev
+
+apt get install -y libpam0g-dev liblz4-dev libseccomp-dev \
+	libreadline-dev libnl-route-3-dev libkrb5-dev libradcli-dev \
+	libcurl4-gnutls-dev libcjose-dev libjansson-dev libprotobuf-c-dev \
+	libtalloc-dev libhttp-parser-dev protobuf-c-compiler gperf \
+	nuttcp lcov libuid-wrapper libpam-wrapper libnss-wrapper \
+	libsocket-wrapper gss-ntlmssp haproxy iputils-ping freeradius \
+	gawk gnutls-bin iproute2 yajl-tools tcpdump
 
 wget https://www.infradead.org/ocserv/download/ocserv-1.1.6.tar.xz
 tar Jxf ocserv-1.1.6.tar.xz
